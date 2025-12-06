8fa77227dfbb66ed0d8314ef1d1f76cd5cc5b0b4
WilderWR7 <wilder.mayta91@gmail.com>
2025-12-06 10:11:34 -0400

    feat(auth): improve JWT validation and enforce user activity status

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -27,7 +27,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     if (!user) throw new UnauthorizedException('Token not valid');
 
-    if (!user.isActive)
+    if (user.deletedAt)
       throw new UnauthorizedException('User is inactive, talk with an admin');
     return user;
   }
